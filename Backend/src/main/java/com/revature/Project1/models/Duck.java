583390b87b1c0c2af71484f305552f93a0937363
mattm <kumkowskimateusz2002@gmail.com>
2024-12-11 14:44:45 -0500

    Created Models of all the classes needed

---
+++ Backend/src/main/java/com/revature/Project1/models/Duck.java
@@ -0,0 +1,57 @@
+package com.revature.Project1.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ducks")
+public class Duck {
+    /*
+    id,
+    reference_id
+    rank
+    nickname
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int reference_id;
+    private int rank;
+    private int nickname;
+
+    public Duck(int rank) {
+        this.rank = rank;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getReference_id() {
+        return reference_id;
+    }
+
+    public void setReference_id(int reference_id) {
+        this.reference_id = reference_id;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(int nickname) {
+        this.nickname = nickname;
+    }
+}
