ec4fe75610bb5f7a15330503f0dbffe8fac565ef
Ritik Patel <ritikp23@gmail.com>
2024-12-11 16:24:26 -0500

    started frontend

---
+++ FrontEnd/mainPage/mainPage.js
@@ -0,0 +1,61 @@
+let duckies = [
+    {
+        id: 1,
+       // referenceId: 1,
+        name: "Duck1",
+        rank: "A",
+
+    },
+    {
+        id: 2,
+       // referenceId: 1,
+        name: "Duck2",
+        rank: "E",
+
+    },
+    {
+        id: 3,
+       // referenceId: 1,
+        name: "Duck3",
+        rank: "D",
+
+    },
+    {
+        id: 4,
+       // referenceId: 1,
+        name: "Duck4",
+        rank: "D",
+
+    },
+    {
+        id: 5,
+       // referenceId: 1,
+        name: "Duck5",
+        rank: "B",
+
+    },
+    {
+        id: 6,
+        //referenceId: 1,
+        name: "Duck6",
+        rank: "F",
+
+    }
+]
+
+let cardContainer = document.getElementById('card-container')
+
+function populateCards(duckies){
+    for(card of duckies){
+        let cDiv = document.createElement('div')
+
+        cDiv.innerHTML = `<p>Name: ${card.name}</p> 
+        <p>Rank: ${card.rank}</p>`
+
+        cDiv.setAttribute('class', 'card')
+
+        cardContainer.append(cDiv)
+    }
+}
+
+populateCards(duckies);
